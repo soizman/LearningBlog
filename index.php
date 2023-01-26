7a6ccaad4d629e3a727ca923c03211add268a715
soizman <popov.nik.cc@gmail.com>
2023-01-26 19:29:49 +0500

    delete connection config

--- index.php
+++ index.php
@@ -4,15 +4,13 @@
     error_reporting(E_ALL);
     ini_set('display_errors', 'on');
     
-    require_once $_SERVER['DOCUMENT_ROOT'] . '/project/config/connection.php';
-    
     spl_autoload_register(function($class) {
-        preg_match('#(.+)\\\\(.+?)$#', $class, $match);
+    preg_match('#(.+)\\\\(.+?)$#', $class, $match);
         
-        $nameSpace = str_replace('\\', DIRECTORY_SEPARATOR, strtolower($match[1]));
-        $className = $match[2];
+    $nameSpace = str_replace('\\', DIRECTORY_SEPARATOR, strtolower($match[1]));
+    $className = $match[2];
         
-        $path = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . $nameSpace . DIRECTORY_SEPARATOR . $className . '.php';
+    $path = $_SERVER['DOCUMENT_ROOT'] . DIRECTORY_SEPARATOR . $nameSpace . DIRECTORY_SEPARATOR . $className . '.php';
         
         if (file_exists($path)) {
             require_once $path;
