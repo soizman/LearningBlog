210a4fb7dbe432e05cf02679955d062e36679b5c
Попов Николай <nikolay.popov@up.ru>
2023-01-30 15:17:33 +0500

    style ArticlePage

--- project/views/articles/articlesPage.php
+++ project/views/articles/articlesPage.php
@@ -1,8 +1,16 @@
 <div class="artic">
-    <?php foreach ($data as $article):?>
-        <h3><?= $article->titleArticle ?></label></h3>
-        <p><?= $article->text ?></p>
-    <?php endforeach ?>
+    <div class="container">
+        <div class="row">
+            <div class="com-md-8">                
+            <?php foreach ($data as $article):?>
+                <div class=" shadow p-3 mb-5 bg-body rounded" style="--bs-bg-opacity: .5;"> 
+                    <h4><?= $article->titleArticle ?></h4>             
+                    <p><?= $article->text ?></p>
+                </div>
+            <?php endforeach ?>
+            </div>
+        </div>
+    </div>
     <h2>Предложить статью</h2>
     <form method="POST" action="/article/new/">        
         <input type="Тема" class="form-control" id="FormInputTitle" placeholder="Тема" name="titleArticle">        
