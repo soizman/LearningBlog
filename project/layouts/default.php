5cf6cc58a89462d3f99f46e861d8723b2d93d4c8
soizman <popov.nik.cc@gmail.com>
2023-02-01 15:10:11 +0500

    create btn auth

--- project/layouts/default.php
+++ project/layouts/default.php
@@ -5,10 +5,8 @@
     <link rel="stylesheet" href="/project/webroot/style.css">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha256-wLz3iY/cO4e6vKZ4zRmo4+9XDpMcgKOvv/zEU3OMlRo=" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://kit.fontawesome.com/e9b54647e9.css" crossorigin="anonymous">    
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha256-m81NDyncZVbr7v9E6qCWXwx/cwjuWDlHCMzi9pjMobA=" crossorigin="anonymous"></script>
-    <script src="https://kit.fontawesome.com/e9b54647e9.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://kit.fontawesome.com/e9b54647e9.css" crossorigin="anonymous">  
     <title><?= $title ?></title><!-- Указать title при переносе шаблона-->
 </head>
 <body>
@@ -27,7 +25,16 @@
                   <a class="nav-link" href="#">Работы</a>
                   <a class="nav-link" href="#">Обратная связь</a>                  
                 </div>
-                <a class="nav-link" href="#" style="padding-left: 10%; color: aliceblue;""><i class="fa-solid fa-user"></i></a>
+
+                <div class="btn-group" style="padding-left: 10%; color: aliceblue;">
+                    <button type="button" class="btn btn-dark dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="fa-solid fa-user"></i>
+                    </button>
+                    <ul class="dropdown-menu">
+                    <li><a class="dropdown-item" href="#">Выпадающая ссылка</a></li>
+                    <li><a class="dropdown-item" href="#">Выпадающая ссылка</a></li>
+                    </ul>
+                </div>               
               </div>
             </div>
           </nav>
@@ -51,7 +58,7 @@
         </div>
     </footer>
   </div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha256-m81NDyncZVbr7v9E6qCWXwx/cwjuWDlHCMzi9pjMobA=" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
 <script src="https://kit.fontawesome.com/e9b54647e9.js" crossorigin="anonymous"></script>
 </body>
 </html>
