3bf7a105c8535950539cac03dce2a7625b63bf07
Попов Николай <nikolay.popov@up.ru>
2023-02-07 12:15:29 +0500

    new form for registration and authentification

--- project/views/auth/loginPage.php
+++ project/views/auth/loginPage.php
@@ -1,1 +1,18 @@
-<h1>Войти</h1>
+<div class="auth">
+    <form method="POST">    
+        <div class="mb-3">
+            <label for="InputEmail" class="form-label">Адрес электронной почты</label>
+            <input type="email" class="form-control" id="InputEmail" aria-describedby="emailHelp" name="email">    
+        </div>
+        <div class="mb-3">
+            <label for="InputPassword1" class="form-label">Пароль</label>
+            <input type="password" class="form-control" id="InputPassword1" name="password">
+        </div>
+        <div class="mb-3 form-check">
+            <input type="checkbox" class="form-check-input" id="Check" name="check">
+            <label class="form-check-label" for="Check">Запомнить меня</label>
+        </div>
+        <button type="submit" class="btn btn-dark" name="submit">Отправить</button>
+        <p>У вас нет аккаунта? - <a href="/registration/">Зарегистрируйтесь</a></p>
+    </form>
+</div>
--- project/views/auth/registPage.php
+++ project/views/auth/registPage.php
@@ -1,1 +1,18 @@
-<h1>Зарегистрироваться</h1>
+<div class="auth">
+    <form method="POST">    
+        <div class="mb-3">
+            <label for="InputEmail" class="form-label">Адрес электронной почты</label>
+            <input type="email" class="form-control" id="InputEmail" aria-describedby="emailHelp" name="email">    
+        </div>
+        <div class="mb-3">
+            <label for="InputPassword1" class="form-label">Пароль</label>
+            <input type="password" class="form-control" id="InputPassword1" name="password">
+        </div>
+        <div class="mb-3">
+            <label for="InputPassword2" class="form-label">Повторите пароль</label>
+            <input type="password" class="form-control" id="InputPassword2" name="password">
+        </div>        
+        <button type="submit" class="btn btn-dark" name="submit">Отправить</button>
+        <p>Уже есть аккаунт? - <a href="/login/">войдите</a></p>
+    </form>
+</div>
